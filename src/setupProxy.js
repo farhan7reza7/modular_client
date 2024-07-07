bdda379d803f5393a0343657d92b82c0ec3d5748
Farhan Reza <fr7reza@gmail.com>
2024-07-07 15:54:41 +0530

    set for backend

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,8 +4,9 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:4000",
+      target: "http://localhost:8080",
       //target: "http://backend-dev.ap-northeast-3.elasticbeanstalk.com/",
+      //target: "http://backend-dev.ap-northeast-3.elasticbeanstalk.com",
       changeOrigin: true,
     })
   );
