83c4f3f774b173e43973631f10036f9897f44777
Farhan Reza <fr7reza@gmail.com>
2024-07-08 00:31:06 +0530

    updated to cognito social auth

---
+++ src/config.js
@@ -0,0 +1,2 @@
+const apiUrl = process.env.REACT_APP_API_URL;
+module.exports = { apiUrl };
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,12 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const { apiUrl } = require("./config");
 
 module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:8080",
+      target: apiUrl,
+      //target: "http://localhost:8080",
       //target: "http://localhost:4000",
 
       //target: "http://backend-dev.ap-northeast-3.elasticbeanstalk.com/",
